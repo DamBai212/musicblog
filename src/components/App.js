983f5d7c848975830a6e7d84b0408f40e2f4d0c0
DamBai212 <damanybailey@icloud.com>
2021-06-22 16:36:53 +0100

    fixed bugs

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FeaturedPost from './FeaturedPost';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        My Music Blog
-      </header>
-    <div className="featuredPost">
-        <FeaturedPost/>
-    </div>
-    <div className="spotifyPlayer">
-      <p>Spotify player here</p>
-    </div>
-    <div className="otherPosts">
-      <p>Other posts</p>
-    </div>
-      <footer className="footer">
-        cosmic footer here
-      </footer>
-
-    </div>
-  );
-}
-
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+       dataReceived: false,      
+       posts: [],
+       authors: [],
+       featuredPostIndex: 0,
+       otherPosts: [],
+    }
+  }
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          My Music Blog
+        </header>
+      <div className="featuredPost">
+          <FeaturedPost/>
+      </div>
+      <div className="spotifyPlayer">
+        <p>Spotify player here</p>
+      </div>
+      <div className="otherPosts">
+        <p>Other posts</p>
+      </div>
+        <footer className="footer">
+          cosmic footer here
+        </footer>
+      </div>
+    );
+  }
+  
+  }
+  
 export default App;
